dc56dfb14ee072140ef0d53ed5213318d73c532d
artur54168 <dsw54168@student.dsw.edu.pl>
2024-07-02 21:07:46 +0200

    poprawki i dodanie log x

--- skrypt.sh
+++ skrypt.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 
-
-
-if [ "$1" != "--logs" ]; then
-    echo "Użycie: $0 --logs"
-    exit 1
-fi
-
-for (( i=1; i<=100; i++ )); do
-    filename="log$i.txt"
-    echo "Nazwa pliku: $filename" > "$filename"
-    echo "Nazwa skryptu: $0" >> "$filename"
-    echo "Data utworzenia: $(date)" >> "$filename"
-done
-echo "Utworzono 100 plików logx.txt"
-
+if [ "$1" = "--logs" ]; then
+    for (( i=1; i<=100; i++ )); do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date)" >> "$filename"
+    done
+    echo "Utworzono 100 plików logx.txt"
 elif [ "$1" = "--date" ]; then
     echo "Dzisiejsza data: $(date)"
+elif [[ "$1" == "--log" && "$2" =~ ^[1-9][0-9]?$|^100$ ]]; then
+    num_files="$2"
+    for (( i=1; i<=$num_files; i++ )); do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date)" >> "$filename"
+    done
+    echo "Utworzono $num_files plików logx.txt"
