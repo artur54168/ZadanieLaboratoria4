01dcdcc499fae504f9b032e1b9e88604d78d6e21
artur54168 <dsw54168@student.dsw.edu.pl>
2024-07-02 20:54:15 +0200

    dodano logx i zmieniono data

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Dzisiejsza data: $(date)"
+
+
+
+if [ "$1" != "--logs" ]; then
+    echo "Użycie: $0 --logs"
+    exit 1
+fi
+
+for (( i=1; i<=100; i++ )); do
+    filename="log$i.txt"
+    echo "Nazwa pliku: $filename" > "$filename"
+    echo "Nazwa skryptu: $0" >> "$filename"
+    echo "Data utworzenia: $(date)" >> "$filename"
+done
+echo "Utworzono 100 plików logx.txt"
+
+elif [ "$1" = "--date" ]; then
+    echo "Dzisiejsza data: $(date)"
